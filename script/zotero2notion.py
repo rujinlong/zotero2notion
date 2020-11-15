7fcedb43af50d43421a15d14fbf3bff04c4f3235
Jinlong Ru <jinlongru@gmail.com>
2020-11-15 20:01:57 +0100

    add new records to notion table

--- script/zotero2notion.py
+++ script/zotero2notion.py
@@ -4,6 +4,9 @@ import click
 import pandas as pd
 import urllib
 import re
+import notion
+from notion.client import NotionClient
+import datetime
 
 
 def file_name(x):
@@ -32,20 +35,37 @@ def add_day_to_date(date):
     return date
 
 
+def add_row(cv, x):
+    row = cv.collection.add_row()
+    row.name = x['Name']
+    row.impact_factor = x['Impact_factor']
+    row.journal = x['Journal']
+    row.pdf = x['PDF']
+    row.url = x['Url']
+    Tadded = [int(i) for i in x['Date_added'].split('-')]
+    Tpublished = [int(i) for i in x['Date_published'].split('-')]
+    row.date_published = notion.collection.NotionDate(datetime.date(Tpublished[0], Tpublished[1], Tpublished[2]))
+    row.date_added = notion.collection.NotionDate(datetime.date(Tadded[0], Tadded[1], Tadded[2]))
+    row.title = x['Title']
+
+
 @click.command()
 @click.option("--zotero_export", '-z', help="zotero_export.csv")
 @click.option("--impact_factor", '-i', help="impact_factor_2020.tsv")
-@click.option("--fout", '-o', help="notion_import.csv")
-def main(zotero_export, impact_factor, fout):
+@click.option("--token", '-t', help="token_v2")
+@click.option("--table_url", '-u', help="table url")
+@click.option('--checkdup/--no-checkdup', default=False, help="Check duplication")
+def main(zotero_export, impact_factor, token, table_url, checkdup):
     """
-    zotero2notion.py -z zotero_export.csv -i impact_factor_2020.tsv -o notion_import.csv
+    zotero2notion.py -z zotero_export.csv -i impact_factor_2020.tsv -t <token_v2> -u <table_url>
     """
+
+    ## Link paper to impact factor
     df = pd.read_csv(zotero_export)
     
     dfif = pd.read_csv(impact_factor, sep='\t')
     dfif.columns = ['journal', "Impact_factor"]
     dfif['journal'] = dfif.apply(lambda x:x['journal'].lower(), axis=1)
-    
     
     df['PDF'] = df.apply(lambda x: pdf_url(x), axis=1)
     df['Name'] = df.apply(lambda x: file_name(x), axis=1)
@@ -56,8 +76,29 @@ def main(zotero_export, impact_factor, fout):
     tbl['Date_added'] = tbl.apply(lambda x:x['Date Added'].split(' ')[0], axis=1)
     tbl['Date_published'] = tbl.apply(lambda x:add_day_to_date(x['Date'].split(' ')[0]), axis=1)
     tbl.rename(columns={"Publication Title": "Journal"}, inplace=True)
-    clms = ['Name', 'Impact_factor', 'Journal', 'PDF', 'Url', 'Date_published', 'Date_added', 'Title']
-    tbl[clms].to_csv(fout, index=False)
+    tbl.Impact_factor.fillna(0, inplace=True)
+    tbl.fillna('', inplace=True)
+    # clms = ['Name', 'Impact_factor', 'Journal', 'PDF', 'Url', 'Date_published', 'Date_added', 'Title']
+    # tbl[clms].to_csv(fout, index=False)
+
+    ## Add to notion
+    recs = tbl.to_dict(orient="records")
+
+    # Get notion table
+    client = NotionClient(token_v2=token)
+    cv = client.get_collection_view(table_url)
+
+    if checkdup:
+        # List existed records
+        recs_exist = [x.name for x in cv.collection.get_rows()]
+        # Delete records which already exists in table
+        recs_add = [x for x in recs if x['Name'] not in recs_exist]
+    else:
+        recs_add = recs
+
+    # Add new rows
+    for rec in recs_add:
+        add_row(cv, rec)
     
 
 if __name__ == '__main__':
